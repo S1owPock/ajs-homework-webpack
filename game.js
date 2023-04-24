fe45a05afbce93173a9de527d3a2543cf8c6d6a9
Max Solodovnikov <maks_solod@list.ru>
2023-04-24 13:06:12 +0300

    game.js

---
+++ game.js
@@ -0,0 +1,27 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+import {Character} from "./domain";
+
+export class Game {}
+
+export class GameSavingData {}
+
+export function readGameSaving ()
+
+export function writeGameSaving ()
+
+
+  
